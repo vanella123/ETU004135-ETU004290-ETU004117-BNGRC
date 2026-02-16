149a4597207748cebddfdc19ce01805c7046fd77
RAELISON ONIMPANAHY Vanella <raelisonvanella@gmail.com>
2026-02-16 23:33:07 +0300

    modele

---
+++ app/views/header.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-expand-lg navbar-dark">
+    <div class="container-fluid">
+        <a class="navbar-brand" href="/">Dons & Besoins</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item"><a class="nav-link" href="/dashboard">Dashboard</a></li>
+                <li class="nav-item"><a class="nav-link" href="/saisieBesoin">Saisie Besoin</a></li>
+                <li class="nav-item"><a class="nav-link" href="/form_dons">Saisie Don</a></li>
+                <li class="nav-item"><a class="nav-link" href="/besoins">Besoins Totaux</a></li>
+                <li class="nav-item"><a class="nav-link" href="/achats">Achats</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
