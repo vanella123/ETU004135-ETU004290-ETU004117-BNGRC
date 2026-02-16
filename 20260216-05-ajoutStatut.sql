e589c4c284a6157dd7943e568e28c653a5c34930
RAELISON ONIMPANAHY Vanella <raelisonvanella@gmail.com>
2026-02-16 21:10:14 +0300

    insert statut

--- 20260216-05-ajoutStatut.sql
+++ 20260216-05-ajoutStatut.sql
@@ -2,5 +2,6 @@ create table statut (
     id_statut int auto_increment primary key,
     libelle varchar(255) not null
 );
+insert into statut (libelle) values ('En attente'), ('En cours'), ('Satisfait');
 alter table besoin add column statut_id int default 1;
 alter table besoin add foreign key (statut_id) references statut(id_statut);
