d7800be5f08c089f70c7696542cee1ed05f095bd
RAELISON ONIMPANAHY Vanella <raelisonvanella@gmail.com>
2026-02-17 08:23:16 +0300

    besoin restant

--- 20260216-05-ajoutStatut.sql
+++ 20260216-05-ajoutStatut.sql
@@ -6,7 +6,7 @@ insert into statut (libelle) values ('En attente'), ('En cours'), ('Satisfait');
 alter table besoin add column statut_id int default 1;
 alter table besoin add foreign key (statut_id) references statut(id_statut);
 
-CREATE VIEW view_resume_besoins AS
+CREATE OR REPLACE VIEW view_resume_besoins AS
 SELECT
     SUM(b.quantite * a.prix_unitaire) AS besoins_totaux,
 
@@ -14,7 +14,8 @@ SELECT
         WHEN b.statut_id = 2 
         THEN b.quantite * a.prix_unitaire
         ELSE 0
-    END) AS besoins_satisfaits,
+    END) AS besoins_satisfaits, 
+    
 
     SUM(CASE 
         WHEN b.statut_id <> 2 OR b.statut_id IS NULL
@@ -23,4 +24,4 @@ SELECT
     END) AS besoins_restants
 
 FROM besoin b 
-JOIN article a ON b.article_id = a.id;
+JOIN article a ON b.article_id = a.id ;
